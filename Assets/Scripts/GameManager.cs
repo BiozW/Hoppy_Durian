2d78aa9a01adbadba1dd21bf56b2cfdd7c2caff2
BiozW <jirayusuk@gmail.com>
2023-08-29 22:43:01 +0700

    Edit: GameManager - clear unuse code

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -28,13 +28,6 @@ public class GameManager : MonoBehaviour
     {
         score++;
     }
-    private void Play()
-    {
-        if(Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0))
-        {
-            Time.timeScale = 1;
-        }
-    }
 
     private void Update()
     {
@@ -44,62 +37,6 @@ public class GameManager : MonoBehaviour
         {
             SceneManager.LoadScene("PlayerScene");
         }
-        if (score > 0)
-        {
-            Debug.Log(score);
-        }
     }
-    /*
-    private Player player;
-    private SpawnerPipe spawner;
-
-    public Text scoreText;
-    public GameObject playButton;
-    public GameObject gameOver;
-    public int score { get; private set; }
-
-    private void Awake()
-    {
-        Application.targetFrameRate = 60;
-
-        player = FindObjectOfType<Player>();
-        spawner = FindObjectOfType<SpawnerPipe>();
-
-        Pause();
-    }
-
-    public void Play()
-    {
-        score = 0;
-        scoreText.text = score.ToString();
-
-        playButton.SetActive(false);
-        gameOver.SetActive(false);
-
-        Time.timeScale = 1f;
-        player.enabled = true;
-        
-    }
-
-    public void GameOver()
-    {
-        playButton.SetActive(true);
-        gameOver.SetActive(true);
-
-        Pause();
-    }
-
-    public void Pause()
-    {
-        Time.timeScale = 0f;
-        player.enabled = false;
-    }
-
-    public void IncreaseScore()
-    {
-        score++;
-        scoreText.text = score.ToString();
-    }
-    */
 
 }
